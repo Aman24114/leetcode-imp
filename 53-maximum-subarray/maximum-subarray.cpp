9f14e0b9a96e88a359f571a8c0a506549c91da3b
Aman24114 <145175881+Aman24114@users.noreply.github.com>
2025-02-17 13:09:14 +0530

    Time: 0 ms (100.00%) | Memory: 71.7 MB (80.70%) - LeetSync

---
+++ 53-maximum-subarray/maximum-subarray.cpp
@@ -0,0 +1,14 @@
+class Solution {
+public:// using kadanes algorithm 
+    int maxSubArray(vector<int>& nums) {
+        int n = nums.size();
+        int maxsum = nums[0];
+        int currentsum = nums[0];
+
+        for (int i = 1; i < n; i++) {
+            currentsum = max(nums[i], currentsum + nums[i]);
+            maxsum = max(maxsum, currentsum);
+        }
+        return maxsum;
+    }
+};
