3e48dbbac256d983d76d036e10f0030c23f7409c
Amanpreet Singh <145175881+Aman24114@users.noreply.github.com>
2025-03-18 19:51:03 +0530

    Time: 36 ms (5.98%) | Memory: 12.7 MB (21.65%) - LeetSync

--- 2134-maximize-the-confusion-of-an-exam/maximize-the-confusion-of-an-exam.cpp
+++ 2134-maximize-the-confusion-of-an-exam/maximize-the-confusion-of-an-exam.cpp
@@ -1,49 +1,21 @@
-// contigous hai to sw socho 
 class Solution {
 public:
     int maxConsecutiveAnswers(string ans, int k) {
-    int n=ans.length();
     int result=0;
-    // f-t case
-    int i=0,j=0;
-    int fcnt=0; 
-    while(j<n)
+    int i=0;
+    int j=0;
+    unordered_map<char,int>mp;
+    while(j<ans.length())
     {
-     if(ans[j]=='F')
-     {
-        fcnt++;
-     }
-     while(fcnt>k)
-     {
-        if(ans[i]=='F')
+        mp[ans[j]]++;
+        while(min(mp['T'],mp['F'])>k) 
         {
-            fcnt--;
+            mp[ans[i]]--;
+            i++;
         }
-        i++;
-     }
-     result=max(result,j-i+1);
-     j++;
-    }
-    // t-f case 
-    int i1=0,j1=0;
-    int tcnt=0; 
-    while(j1<n)
-    {
-     if(ans[j1]=='T')
-     {
-        tcnt++;
-     }
-     while(tcnt>k)
-     {
-        if(ans[i1]=='T')
-        {
-            tcnt--;
-        }
-        i1++;
-     }
-     result=max(result,j1-i1+1);
-     j1++;
-    }
+        result=max(result,j-i+1);
+        j++;
+    }    
     return result;
     }
 };
