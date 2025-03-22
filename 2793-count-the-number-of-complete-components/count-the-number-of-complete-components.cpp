319ce2cb36a0bd8937e222b73a0611958d57e514
Amanpreet Singh <145175881+Aman24114@users.noreply.github.com>
2025-03-22 09:59:52 +0530

    Time: 46 ms (64.81%) | Memory: 131.6 MB (58.87%) - LeetSync

--- 2793-count-the-number-of-complete-components/count-the-number-of-complete-components.cpp
+++ 2793-count-the-number-of-complete-components/count-the-number-of-complete-components.cpp
@@ -1,36 +1,71 @@
-class Solution {
-public:
-    void dfs(int i,vector<vector<int>>&graph,vector<bool>&vis,int &node,int &sum)
+class DSU
+{
+    vector<int>parent,size;
+    public:
+    DSU(int n)
     {
-        vis[i]=true;
-        node++;
-        sum+=graph[i].size();
-        for(int adj:graph[i])
+        parent.resize(n+1);
+        size.resize(n+1);
+        for(int i=0;i<=n;i++)
         {
-            if(!vis[adj])
-            {
-             dfs(adj,graph,vis,node,sum);
-            }
+            parent[i]=i;
+            size[i]=1;
         }
     }
+    int findpar(int node)
+    {
+        if(node==parent[node])
+        {
+            return node;
+        }
+        return parent[node]=findpar(parent[node]);
+    }
+    
+    void Union(int u,int v)
+    {
+        int pu = findpar(u);
+        int pv = findpar(v);
+        if (pu == pv)
+          return ;
+        if (size[pu] < size[pv]) {
+            parent[pu] = pv;
+            size[pv] += size[pu];
+        } else {
+            parent[pv] = pu;
+            size[pu] += size[pv];
+        }
+    }
+    int getsize(int node) { return size[node]; }
+};
+
+class Solution {
+public:
     int countCompleteComponents(int n, vector<vector<int>>& edges) {
-    vector<vector<int>>graph(n);
-    for(auto ed:edges)
-    {
-        graph[ed[0]].push_back(ed[1]);
-        graph[ed[1]].push_back(ed[0]);
-    }
-    vector<bool>vis(n,0);
-    int ans=0;
-    for(int i=0;i<n;i++)
-    {
-        if(vis[i])
-        continue;
-        int node=0,sum=0;
-        dfs(i,graph,vis,node,sum);
-        if(sum == node*(node-1))
-        ans++;
-    }
+      DSU ds(n);
+      for(auto it:edges)
+      {
+        ds.Union(it[0],it[1]);
+      }  
+      unordered_map<int,int>mp;
+      for(auto it:edges)
+      {
+        mp[ds.findpar(it[0])]++;
+      }
+      for(int i=0;i<n;i++)
+      {
+        if(mp.find(ds.findpar(i))==mp.end())
+        mp[i]=0;
+      }
+     int ans=0;
+     for(auto it:mp)
+     {
+        int a=ds.getsize(it.first);
+        int b=a*(a-1)/2;
+        if(b==it.second)
+        {
+            ans++;
+        }
+     }
      return ans;
     }
 };
