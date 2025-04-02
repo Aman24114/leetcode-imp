033437080c51959b6b30701baa865de6b1f34523
Amanpreet Singh <145175881+Aman24114@users.noreply.github.com>
2025-04-03 01:20:39 +0530

    Time: 0 ms (100.00%) | Memory: 91.1 MB (85.24%) - LeetSync

---
+++ 3152-maximum-value-of-an-ordered-triplet-ii/maximum-value-of-an-ordered-triplet-ii.cpp
@@ -0,0 +1,19 @@
+#define ll long long 
+class Solution {
+public:
+    ll maximumTripletValue(vector<int>& nums) {
+    int n=nums.size();
+    if(n<3)
+    return 0;
+    ll maxr=0;
+    ll maxi=nums[0];
+    ll maxdif=0;
+    for(int i=1;i<n;i++)
+    {
+        maxr=max(maxr,(ll)maxdif*nums[i]);
+        maxdif=max(maxdif,(ll)maxi-nums[i]);
+        maxi= max(maxi,(ll)nums[i]);;
+    } 
+    return maxr;  
+    }
+};
