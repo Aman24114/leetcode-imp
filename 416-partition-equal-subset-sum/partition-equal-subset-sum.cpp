f00db153e2980ec6011a928f7fbdd7f607677180
Amanpreet Singh <145175881+Aman24114@users.noreply.github.com>
2025-04-07 17:13:02 +0530

    Time: 139 ms (35.92%) | Memory: 17.1 MB (62.07%) - LeetSync

--- 416-partition-equal-subset-sum/partition-equal-subset-sum.cpp
+++ 416-partition-equal-subset-sum/partition-equal-subset-sum.cpp
@@ -1,41 +1,45 @@
 class Solution {
 public:
+    bool f(int n,int target,vector<int>&nums)
+    {
+        vector<vector<bool>>dp(n,vector<bool>(target+1,false));
+        for(int i=0;i<n;i++)
+        {
+          dp[i][0]=true;
+        }
+          if(nums[0]<=target)
+          {
+            dp[0][nums[0]]=true;
+          }  
+          for(int i=1;i<n;i++)
+          {
+            for(int t=1;t<=target;t++)
+            {
+                bool nt=dp[i-1][t];
+                bool take=false;
+                if(nums[i]<=t)
+                {
+                    take=dp[i-1][t-nums[i]];
+                }
+                dp[i][t]=nt||take;
+            }
+          }
+         return dp[n-1][target];
+    }
     bool canPartition(vector<int>& nums) {
-    int n= nums.size();
     int sum=0;
-    for(int i=0;i<n;i++)
+    for(int i=0;i<nums.size();i++)
     {
-     sum+=nums[i];
-    }
-    if(sum%2==1)
+        sum+=nums[i];
+    } 
+    if(sum %2==1)
+    return false;
+    int target= sum/2;
+    int n=nums.size();
+    if(f(n,target,nums))
     {
-     return false;
+        return true;
     }
-    else
-    {
-    int k= sum/2;
-    vector<vector<bool>>dp(n,vector<bool>(k+1,false));
-    for(int i=0;i<n;i++)
-    {
-        dp[i][0]=true; // frist col true 
-    }
-    if(nums[0]<=k)
-    dp[0][nums[0]]=true;
-        for(int i=1;i<n;i++)
-        {
-        for(int t=1;t<=k;t++)
-        {
-        bool nt= dp[i-1][t];
-        bool ta= false;
-        if(nums[i]<=t)
-        {
-        ta=dp[i-1][t-nums[i]];
-        }
-        dp[i][t]=nt||ta;
-        }
-        }
-    return dp[n-1][k];
-    }
-    return false; 
+    return false;
     }
 };
