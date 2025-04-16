b80036c83140f2b8cf1e56edc50b92aa9f3704fa
Amanpreet Singh <145175881+Aman24114@users.noreply.github.com>
2025-04-17 02:28:37 +0530

    Time: 79 ms (68.32%) | Memory: 79.1 MB (54.61%) - LeetSync

--- 2626-count-the-number-of-good-subarrays/count-the-number-of-good-subarrays.cpp
+++ 2626-count-the-number-of-good-subarrays/count-the-number-of-good-subarrays.cpp
@@ -1,31 +1,26 @@
-#define ll long long 
+#define ll long long
 class Solution {
 public:
-    long long countGood(vector<int>& nums, int k) {
-    int n=nums.size();
-    ll ans=0;
-    unordered_map<int,int>mp;
-    ll pairs=0;
-    ll l=0,r=-1;
-    while(l<n || r+1<n)
-    {
-        while(r+1<n && pairs<k)
-        {
-            r++;
-            mp[nums[r]]++;
-            if(mp[nums[r]]>1)
-            {
-                pairs+=mp[nums[r]]-1; // kitna pair banega
-            }
-        }
-        if(pairs>=k)
-        {
-            ans+=(n-r);// n-r tak ka subarray phir slngidn window lagake shift karenge
-        }
-        mp[nums[l]]--;
-        pairs-=mp[nums[l]];
-        l++;
-    } 
-    return ans;   
+    ll countGood(vector<int>& nums, int k)
+     {
+      int n=nums.size();
+      int i=0,j=0;
+      ll result=0;
+      ll pairs=0;
+      unordered_map<int,int>mp;
+      while(j<n)
+      {
+       pairs+=mp[nums[j]];
+       mp[nums[j]]++;
+       while(pairs>=k)
+       {
+        result+=(n-j);
+        mp[nums[i]]--;
+        pairs-=mp[nums[i]];
+        i++;
+       }
+       j++;
+     } 
+     return result; 
     }
 };
