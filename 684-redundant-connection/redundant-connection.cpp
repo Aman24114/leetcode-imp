a079faf9002761db26ef11534d6fa74b70bd12b5
Amanpreet Singh <145175881+Aman24114@users.noreply.github.com>
2025-04-18 22:04:18 +0530

    Time: 0 ms (100.00%) | Memory: 12.7 MB (98.39%) - LeetSync

--- 684-redundant-connection/redundant-connection.cpp
+++ 684-redundant-connection/redundant-connection.cpp
@@ -1,44 +1,62 @@
+class DisjointSet
+{   private:
+    vector<int>parent,rank;
+    public:
+    DisjointSet(int n)
+    {
+        for(int i=0;i<=n;i++)
+        {
+            rank.push_back(0);
+            parent.push_back(i);
+        }
+    }
+    int findparent(int x)
+    {
+        if(x==parent[x])
+        {
+        return x;
+        }
+        return parent[x]=findparent(parent[x]);
+    }
+    void unionbyrank(int u,int v)
+    {
+        int u1=findparent(u);
+        int v1=findparent(v);
+        if(rank[u1]>=rank[v1])
+        {
+            rank[u1]+=rank[v1];
+            parent[v1]=u1;
+        }
+        else if(rank[u1]<rank[v1])
+        {
+            rank[v1]+=rank[u1];
+            parent[u1]=v1;
+        }
+        else
+        {
+            rank[v1]+=rank[u1];
+            parent[u1]=v1;
+        }
+    }
+};
 class Solution {
 public:
-class DisjointSet{
-    public:
-        vector<int> parent, size;
-        DisjointSet (int n){
-            size.resize(n+1, 1);
-            parent.resize(n+1);
-            for(int i=0; i<=n; i++) parent[i] = i;
+    vector<int> findRedundantConnection(vector<vector<int>>& edges)
+    {
+       int n=edges.size();
+       DisjointSet ds= DisjointSet(n);
+       vector<int>ans;
+       for(int i=0;i<n;i++)
+       {
+        if((ds.findparent(edges[i][0]))!=ds.findparent(edges[i][1]))
+        {
+            ds.unionbyrank(edges[i][0],edges[i][1]);
         }
-
-        int findUPar(int node){
-            if(node == parent[node]) return node;
-            return parent[node] = findUPar(parent[node]);
+        else
+        {
+            ans=edges[i];
         }
-        bool findParEqual (int u, int v){
-            return findUPar(u)==findUPar(v);
-        }
-        void unionBySize (int u, int v){
-            int ulp_u = findUPar(u), ulp_v = findUPar(v);
-            if(ulp_u == ulp_v) return;
-            if(size[ulp_u]<size[ulp_v]){
-                parent[ulp_u] = ulp_v;
-                size[ulp_v] += size[ulp_u];
-            } else{
-                parent[ulp_v] = ulp_u;
-                size[ulp_u] += size[ulp_v];
-            }
-        }
-};
-// reduntant connection// using disjoint set union 
- vector<int> findRedundantConnection(vector<vector<int>>& edges) {
-int n=edges.size();
-DisjointSet ds(n);
-vector<vector<int>> ans;
-for(auto it: edges)
-{
-    if (ds.findParEqual(it[0],it[1]))
-    ans.push_back(it);
-    ds.unionBySize(it[0],it[1]);
-}
-return ans.back();
- }
+       }
+        return ans;   
+    }
 };
