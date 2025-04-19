7a39de762d726d15e8157b1414e61cf5a9ba899c
Amanpreet Singh <145175881+Aman24114@users.noreply.github.com>
2025-04-19 11:46:10 +0530

    Time: 603 ms (90.72%) | Memory: 0B (100.00%) - LeetSync

---
+++ 1292-immediate-food-delivery-ii/immediate-food-delivery-ii.sql
@@ -0,0 +1,8 @@
+Select round(avg(order_date=customer_pref_delivery_date)*100,2)as immediate_percentage
+FROM Delivery
+WHERE (customer_id,order_date)in
+(
+        SELECT customer_id,min(order_date)
+        FROM Delivery 
+        group by customer_id
+)
