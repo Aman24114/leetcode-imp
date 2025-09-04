0d3de68309ebe148c8a48370d501d6df7a53f2a1
Amanpreet Singh <145175881+Aman24114@users.noreply.github.com>
2025-09-04 10:24:55 +0530

    Time: 0 ms (100.00%) | Memory: 8.9 MB (16.70%) - LeetSync

---
+++ 60-permutation-sequence/permutation-sequence.cpp
@@ -0,0 +1,25 @@
+class Solution {
+public:
+    string getPermutation(int n, int k) {
+    vector<int>num;
+    int f=1;
+    for(int i=1;i<n;i++)
+    {
+        f=f*i;
+        num.push_back(i);
+    }    
+    num.push_back(n);
+    string ans="";
+    k=k-1;
+    while(1)
+    {
+      ans=ans+to_string(num[k/f]);
+      num.erase(num.begin()+k/f);
+      if(num.size()==0)
+      break;
+      k=k%f;
+      f=f/num.size();
+    }
+    return ans;
+    }
+};
