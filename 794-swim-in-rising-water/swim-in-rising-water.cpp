b2011be12989f39ef5f022d152baefe6bd084d7f
Amanpreet Singh <145175881+Aman24114@users.noreply.github.com>
2025-10-06 10:33:20 +0530

    Time: 5 ms (70.97%) | Memory: 12.7 MB (86.05%) - LeetSync

--- 794-swim-in-rising-water/swim-in-rising-water.cpp
+++ 794-swim-in-rising-water/swim-in-rising-water.cpp
@@ -1,10 +1,11 @@
 class Solution {
 public:
+    typedef pair<int,pair<int,int>> p;
     int swimInWater(vector<vector<int>>& grid) {
     int n=grid.size();
     int m=grid[0].size();
     vector<vector<int>>dist(n,vector<int>(m,1e9));
-    priority_queue<pair<int,pair<int,int>>,vector< pair<int,pair<int,int>>>,greater<pair<int,pair<int,int>>>> pq;    
+    priority_queue<p,vector<p>,greater<p>> pq;// min heap lenge    
     int time=0;
     dist[0][0]=grid[0][0];
     pq.push({grid[0][0],{0,0}});
@@ -18,29 +19,27 @@ public:
         int r=it.second.first;
         int c= it.second.second;
         if(r==n-1 && c==m-1)
+        {
+            return time;
+        }
+        for(int ind=0;ind<4;ind++)
+        {
+        int nrow=r+dr[ind];
+        int ncol=c+dc[ind];
+        if(nrow>=0 && ncol>=0 && nrow<n && ncol<m)
+        { 
+            int newtime=grid[nrow][ncol]-time;
+            if(newtime>0)
+            newtime=newtime+time;
+            else
+            newtime=time;
+            if(dist[nrow][ncol]>newtime)
             {
-                return time;
+                dist[nrow][ncol]=newtime;
+                pq.push({newtime,{nrow,ncol}});
             }
-         for(int ind=0;ind<4;ind++)
-         {
-            int nrow=r+dr[ind];
-            int ncol=c+dc[ind];
-            if(nrow >= 0 && ncol >= 0 && nrow < n && ncol < m )
-            { 
-                int newtime=grid[nrow][ncol]-time;
-                if(newtime>0)
-                newtime=newtime+time;
-                else
-                {
-                    newtime=time;
-                }
-                if(dist[nrow][ncol]>newtime)
-                {
-                    dist[nrow][ncol]=newtime;
-                    pq.push({newtime,{nrow,ncol}});
-                }
-            }
-         }
+        }
+        }
     }
     return -1;
     }
